f4dcd8cc4ffb4accb0ea0596f37f723fc230dbac
twinkle-mahato <twinklemahato8670@gmail.com>
2026-02-08 09:48:41 +0530

    Routing Test

--- src/app.js
+++ src/app.js
@@ -2,15 +2,30 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("Hello From Server!!!");
+app.use("/hello/2", (req, res) => {
+  res.send("Abra Ka Dabra");
 });
 
 app.use("/hello", (req, res) => {
   res.send("Hello Hello Hello!!!");
 });
 
+// "/hello" over write the "/hello/2" that is why we go to this path it alwasy showing hello hello hello!! so we need to write this route "/hello/2" before "/hello" so it wont overwrite
+
+// app.use("/hello/2", (req, res) => {
+//   res.send("Abra Ka Dabra");
+// });
+
+app.use("/test", (req, res) => {
+  res.send("Hello from server!!!");
+});
+
+app.use("/", (req, res) => {
+  res.send("Namaste Akshay!!!");
+});
+
 
 app.listen(7777, () => {
   console.log("Server Listening on port 7777");
 });
+
