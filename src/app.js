a7bccb062ded174a477441fd99fad6f1331546c3
twinkle-mahato <twinklemahato8670@gmail.com>
2026-02-08 19:56:38 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -2,27 +2,32 @@ const express = require("express");
 
 const app = express();
 
-// This will only  handle GET call  to /user
-app.get("/user", (req, res) => {
-  res.send({
-    firstname: "Twinkle",
-    lastName: "Mahato",
-  });
-});
-
-app.post("/user", (req, res) => {
-  // data save successfully
-  res.send("Data successfully send to the database !!");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("Deleted Successfully");
-})
-
-// This Match all the HTTP methods API calls to /test
-app.use("/test", (req, res) => {
-  res.send("Hello from server!!!");
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    //Route Handler
+    console.log("handling the route user 1 !!");
+    //res.send("Response");
+    next();
+  },
+  (req, res, next) => {
+    //route handler
+    console.log("handling the route user 2 !!");
+    //res.send("Response 2");
+    next();
+  },
+  (req, res, next) => {
+    //route handler
+    console.log("handling the route user 2 !!");
+    //res.send("Response 3");
+    next();
+  },
+  (req, res, next) => {
+    //route handler
+    console.log("handling the route user 2 !!");
+    res.send("Response 4");
+  },
+);
 
 app.listen(7777, () => {
   console.log("Server Listening on port 7777");
