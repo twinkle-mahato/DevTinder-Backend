ff545ba55860c5d3a71acc603cabf05cf3eb54e0
twinkle-mahato <twinklemahato8670@gmail.com>
2026-02-10 21:02:28 +0530

    GET APIs

--- src/app.js
+++ src/app.js
@@ -7,6 +7,48 @@ const User = require("./models/user");
 //our middleware now be activated for all the routes
 app.use(express.json());
 
+//Feed API -> GET/feed -get all the users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//GET more than one user have same email and we want to find only one user
+app.get("/user", async (req, res) => {
+  const userEmail = req?.body?.emailId;
+
+  try {
+    console.log(userEmail);
+    const user = await User.findOne({ emailId: userEmail });
+    if (!user) {
+      res.status(404).send("User Not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//GET user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req?.body?.emailId;
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("User Not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
 app.post("/signup", async (req, res) => {
   //creating a new instance of user model
   console.log(req.body);
