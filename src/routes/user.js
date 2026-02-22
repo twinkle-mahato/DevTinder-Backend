842ba148bdc23bf084fb1b8191d46a38f9c8c7b2
twinkle-mahato <twinklemahato8670@gmail.com>
2026-02-22 19:07:40 +0530

    created API of getting all the connection which is accepted

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,7 +3,14 @@ const { userAuth } = require("../middlewares/auth");
 const userRouter = express.Router();
 const connectionRequestModel = require("../models/connectionRequest");
 
-// get all the pending connection request for the loggedIn user
+// get all the pending connection request for the loggedIn user received
+
+// This API returns all connection requests
+// where the logged-in user is the receiver
+// and the status is still "interested" (pending)
+
+const USER_SAFE_DATA = "firstName lastName photoUrl gender skills";
+
 userRouter.get("/user/request/received", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
@@ -13,11 +20,48 @@ userRouter.get("/user/request/received", userAuth, async (req, res) => {
         toUserId: loggedInUser._id,
         status: "interested",
       })
-      .populate("fromUserId", ["firstName", "lastName","phtoUrl","gender","skills"]);
-
+      .populate("fromUserId", USER_SAFE_DATA);
+    //Send response back to client
     res.json({
       message: "Data fetched successfully!!",
       data: connectionRequest,
+    });
+  } catch (err) {
+    res.status(400).send("Error:" + err.message);
+  }
+});
+
+//Get all the connection which is accepted
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    //get logged in user
+    const loggedInUser = req.user;
+    //Find accepted connections where user is sender or receiver
+    const connectionRequests = await connectionRequestModel
+      .find({
+        $or: [
+          { toUserId: loggedInUser._id, status: "accepted" },
+          { fromUserId: loggedInUser._id, status: "accepted" },
+        ],
+      })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
+
+    console.log(connectionRequests);
+
+    // Extract only the connected user's profile from each accepted connection.
+    // If the logged-in user is the sender (fromUserId),
+    // return the receiver (toUserId).
+    // Otherwise, return the sender (fromUserId).
+    const data = connectionRequests.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
+    });
+    //send response
+    res.json({
+      data,
     });
   } catch (err) {
     res.status(400).send("Error:" + err.message);
