055a6d8e9b027624b7a8b9a951cebab83bee8729
twinkle-mahato <twinklemahato8670@gmail.com>
2026-05-10 08:58:03 +0530

    fix: add sameSite and secure flags to cookies for cross-domain auth

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -31,6 +31,9 @@ authRouter.post("/signup", async (req, res) => {
 
     res.cookie("token", token, {
       expires: new Date(Date.now() + 8 * 3600000),
+      httpOnly: true,
+      sameSite: "None",
+      secure: true,
     });
 
     res.json({ message: "User registered successfully!", data: savedUser });
@@ -61,6 +64,9 @@ authRouter.post("/login", async (req, res) => {
 
       res.cookie("token", token, {
         expires: new Date(Date.now() + 8 * 3600000),
+        httpOnly: true,
+        sameSite: "None",
+        secure: true,
       }); // here we pass the cookie name as a token and then pass the token
       res.send(user);
     } else {
@@ -74,6 +80,9 @@ authRouter.post("/login", async (req, res) => {
 authRouter.post("/logout", async (req, res) => {
   await res.cookie("token", null, {
     expires: new Date(Date.now()), //i am setting the expiry time as the current time
+    httpOnly: true,
+    sameSite: "None",
+    secure: true,
   });
   res.send("Logged out successfully");
 });
